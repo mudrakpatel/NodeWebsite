c840c6660c1ddaf8c4100a941db40c1fec6eca86
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-21 12:16:20 -0400

    Custom responsive navbar done :)

--- public/Scripts/navbar.js
+++ public/Scripts/navbar.js
@@ -1,3 +1,5 @@
+//Mudrak Patel
+//Github : mudrakpatel
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
