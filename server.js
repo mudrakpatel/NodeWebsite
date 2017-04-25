a8ffd704af73a13fefdec74b0a83c0e9bc732b3f
mudrakpatel <mudrakpatel96@gmail.com>
2017-04-24 20:43:12 -0400

    Data passing attempt 1 failed

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.set('view engine', 'ejs');
 //set Templates directory as views folder for ejs view engine
 app.set('views', path.join(__dirname, 'public/Templates'));
 //Importing data from model.json file
-var data = require("./public/model.json");
-app.locals.appData = data;
+//app.locals.appData = require("./public/model.json");
 //Code to use static files and Routes folder contents
 app.use(express.static("./public"));
 app.use(require("./public/Routes/index"));
