4c742838e3bd23eaf8cbbfb2f853aff5acc1338c
mudrakpatel <mudrakpatel96@gmail.com>
2017-04-30 00:34:44 -0400

    Data passing works; MVC implementation works; Styling time

--- public/Routes/character.js
+++ public/Routes/character.js
@@ -5,11 +5,13 @@ var characterData = require("../model.json");
 router.get("/character.ejs/:characterID", function(request, response) {
     var tempCharacter = {};
     characterData.charactersArray.forEach(function(character) {
-        if (request.params.characterID === character.characterID) {
+        if (character.characterID == request.params.characterID) {
             tempCharacter = character;
         }
     });
-    response.render("character.ejs", { routeCharacter: tempCharacter });
+    response.render("character.ejs", {
+        tempCharacter: tempCharacter
+    });
 });
 
 module.exports = router;
