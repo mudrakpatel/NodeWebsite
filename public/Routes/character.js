1707d84260ecd2f566a348075769e513a5139f93
mudrakpatel <mudrakpatel96@gmail.com>
2017-04-29 23:52:19 -0400

    CSS path error solved in header.ejs; Data not passing into views through URL

--- public/Routes/character.js
+++ public/Routes/character.js
@@ -1,8 +1,15 @@
 var express = require("express");
 var router = express.Router();
+var characterData = require("../model.json");
 
-router.get("/characters.ejs", function(request, response) {
-    response.render("characters.ejs");
+router.get("/character.ejs/:characterID", function(request, response) {
+    var tempCharacter = {};
+    characterData.charactersArray.forEach(function(character) {
+        if (request.params.characterID === character.characterID) {
+            tempCharacter = character;
+        }
+    });
+    response.render("character.ejs", { routeCharacter: tempCharacter });
 });
 
 module.exports = router;
--- public/Routes/characters.js
+++ public/Routes/characters.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/character.ejs", function(request, response) {
-    response.render("character.ejs");
+router.get("/characters.ejs", function(request, response) {
+    response.render("characters.ejs");
 });
 
 module.exports = router;
