8e98ea99171aa61c40468244656f238c3fa59cf2
mudrakpatel <mudrakpatel96@gmail.com>
2017-04-30 01:23:33 -0400

    character.js code comments

--- public/Routes/character.js
+++ public/Routes/character.js
@@ -4,11 +4,17 @@ var characterData = require("../model.json");
 
 router.get("/character.ejs/:characterID", function(request, response) {
     var tempCharacter = {};
+    //Loop through all characters in the array
     characterData.charactersArray.forEach(function(character) {
         if (character.characterID == request.params.characterID) {
+            //If condition is true,
+            //then store the parameter object
+            //in the tempCharacter variable object
             tempCharacter = character;
         }
     });
+    //Render the character.ejs template with the requested
+    //character information passed to character.ejs template
     response.render("character.ejs", {
         tempCharacter: tempCharacter
     });
