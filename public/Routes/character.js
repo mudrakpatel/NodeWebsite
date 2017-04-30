65e8324e4bde6fc5d322cebd486c61bd1771601f
mudrakpatel <mudrakpatel96@gmail.com>
2017-04-30 02:45:49 -0400

    character.js => added a 404 handler

--- public/Routes/character.js
+++ public/Routes/character.js
@@ -2,14 +2,20 @@ var express = require("express");
 var router = express.Router();
 var characterData = require("../model.json");
 
+//Unknown route handler
+router.get("/character.ejs", function(request, response) {
+    response.status(404);
+    response.render("error404.ejs");
+    response.end();
+});
+
+//specific character route handler
 router.get("/character.ejs/:characterID", function(request, response) {
     var tempCharacter = {};
     //Loop through all characters in the array
     characterData.charactersArray.forEach(function(character) {
         if (character.characterID == request.params.characterID) {
-            //If condition is true,
-            //then store the parameter object
-            //in the tempCharacter variable object
+            //If true, assign tempCharacter to parameter object
             tempCharacter = character;
         }
     });
