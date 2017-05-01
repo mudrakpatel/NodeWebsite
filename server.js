180dbc40623b564a68af86b7a808821cb267714b
mudrakpatel <mudrakpatel96@gmail.com>
2017-05-01 00:20:51 -0400

    Added code to optimize listening port

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.locals.characterData = require("./public/model.json");
 app.set('view engine', 'ejs');
 //set Templates directory as views folder for ejs view engine
 app.set('views', path.join(__dirname, 'public/Templates'));
+app.set("port", process.env.PORT || 3000);
 
 //Code to use static files and Routes folder contents
 app.use(express.static("./public"));
@@ -18,9 +19,9 @@ app.use(require("./public/Routes/characters"));
 app.use(require("./public/Routes/character"));
 
 //specify a port number to listen for server
-app.listen(3000);
+app.listen(app.get("port"));
 //log the server status to the console
-console.log("Character World app server running on port 3000...");
+console.log("Character World app server running on port " + app.get("port") + "...");
 
 //Export this file as a module
 module.exports = app;
