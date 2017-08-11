5c77cda0ed5c83138474fc5cb105f7c32241f741
mudrakpatel <mudrakpatel96@gmail.com>
2017-08-10 21:52:05 -0400

    W3Schools approach trial

--- public/Scripts/emailScript.js
+++ public/Scripts/emailScript.js
@@ -1,6 +1,6 @@
 //nodemailer and xoauth2 imports
 var nodemailer = require("nodemailer");
-var xoauth2 = require("xoauth2");
+//var xoauth2 = require("xoauth2");
 //other variables
 var userEmail = document.getElementById("fromTextBox");
 
@@ -8,20 +8,22 @@ var userEmail = document.getElementById("fromTextBox");
 var transporter = nodemailer.createTransport({
     service: "gmail",
     auth:{
-            xoauth2: xoauth2.createXOAuth2Generator({
-                user: userEmail.innerText,
-                clientId: "",
-                clientSecret: "",
-                refreshToken: ""
-            })
+            // xoauth2: xoauth2.createXOAuth2Generator({
+            //     user: userEmail.innerText,
+            //     clientId: "",
+            //     clientSecret: "",
+            //     refreshToken: ""
+            // })
+            user: "testpurposemudrak@gmail.com",
+            pass: "Mudrak@123"
     }
 });
 
 //mailOptions object
 var mailOptions = {
-    from: userEmail.innerText,
-    to: "testpurposemudrak@gmail.com",
-    subject: "Contact via characterworld.",
+    from: "testpurposemudrak@gmail.com",
+    to: "mudrakpatel02@gmail.com",
+    subject: "Via characterworld from " + userEmail.innerText,
     text: document.getElementById("emailBodyTextArea").innerText
 };//try running by removing semicolon if errors/exceptions occur
 
